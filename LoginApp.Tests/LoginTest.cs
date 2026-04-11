4d8a05185e40b961a716c9a3d1f591806abbae7e
Rakhi Sandheesh <143313680+rakhitk@users.noreply.github.com>
2026-04-11 16:21:23 +0530

    Add project files.

---
+++ LoginApp.Tests/LoginTest.cs
@@ -0,0 +1,41 @@
+﻿namespace LoginApp.Tests
+{
+    using Microsoft.Playwright;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public async Task Login_Should_Work()
+        {
+            using var playwright = await Playwright.CreateAsync();
+
+            var browser = await playwright.Chromium.LaunchAsync(new()
+            {
+                Headless = false // browser visible
+            });
+
+            var context = await browser.NewContextAsync(new()
+            {
+                IgnoreHTTPSErrors = true
+            });
+
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://localhost:5001/login");
+
+            await page.FillAsync("[data-testid='username']", "admin");
+            await page.FillAsync("[data-testid='password']", "1234");
+
+            await page.ClickAsync("[data-testid='login-btn']");
+
+            await page.WaitForSelectorAsync("text=Login Success");
+
+            Assert.IsTrue(await page.IsVisibleAsync("text=Login Success"));
+
+            await browser.CloseAsync();
+        }
+    }
+}
